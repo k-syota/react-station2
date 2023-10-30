13fc24df3be3d75330a489f1dacca4678d04f810
西村翔太 <nishimurasyota@k-syota.local>
2023-10-30 23:08:50 +0900

    :sparkles: Axisoでのデータ取得処理

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-// import axios from "axios";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  // const getPostLists = axios.get()
+  useEffect(() => {
+    const getPostLists = axios
+      .get("https://railway.bulletinboard.techtrain.dev/threads?offset=1")
+      .then((data) => {
+        setThreads(data.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+  const [threads, setThreads] = useState([]);
+  console.log(threads);
   return (
     <>
       <div className="p-12 h-screen bg-gray-500">
@@ -11,18 +23,11 @@ function App() {
         <div className="text-center p-4">
           <p className="text-xl p-5">一覧</p>
           <ul>
-            <li className="font-bold p-2 cursor-pointer mb-2 bg-slate-200 rounded-md">
-              テスト１
-            </li>
-            <li className="font-bold p-2 cursor-pointer mb-2 bg-slate-200 rounded-md">
-              テスト１
-            </li>
-            <li className="font-bold p-2 cursor-pointer mb-2 bg-slate-200 rounded-md">
-              テスト１
-            </li>
-            <li className="font-bold p-2 cursor-pointer mb-2 bg-slate-200 rounded-md">
-              テスト１
-            </li>
+            {threads.map((t) => (
+              <li className="font-bold p-2 cursor-pointer mb-2 bg-slate-200 rounded-md">
+                {t.title}
+              </li>
+            ))}
           </ul>
         </div>
       </div>
