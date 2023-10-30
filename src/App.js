0e972b92d2d6138a78baa84876f5f079b233a948
西村翔太 <nishimurasyota@k-syota.local>
2023-10-30 22:56:53 +0900

    :lipstick: デザインの追加

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import axios from "axios";
 
 function App() {
+  // const getPostLists = axios.get()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="p-12 h-screen bg-gray-500">
+        <header className="text-center font-serif font-bold text-4xl">
+          掲示板
+        </header>
+        <div className="text-center p-4">
+          <p className="text-xl p-5">一覧</p>
+          <ul>
+            <li className="font-bold p-2 cursor-pointer mb-2 bg-slate-200 rounded-md">
+              テスト１
+            </li>
+            <li className="font-bold p-2 cursor-pointer mb-2 bg-slate-200 rounded-md">
+              テスト１
+            </li>
+            <li className="font-bold p-2 cursor-pointer mb-2 bg-slate-200 rounded-md">
+              テスト１
+            </li>
+            <li className="font-bold p-2 cursor-pointer mb-2 bg-slate-200 rounded-md">
+              テスト１
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
