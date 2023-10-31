49d4dc7ebef106a88fccce658af31e3fa6ee24a5
西村翔太 <nishimurasyota@k-syota.local>
2023-10-31 16:35:39 +0900

    :adhesive_bandage: 諸々作業
    スレッド作成のボタン作成、map関数のkey指定、スレッド取得関数の修正

--- src/App.js
+++ src/App.js
@@ -2,29 +2,47 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  useEffect(() => {
-    const getPostLists = axios
-      .get("https://railway.bulletinboard.techtrain.dev/threads?offset=1")
-      .then((data) => {
-        setThreads(data.data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }, []);
   const [threads, setThreads] = useState([]);
-  console.log(threads);
+  const [error, setError] = useState(null); // エラーステートを追加
+
+  useEffect(() => {
+    // 非同期関数を定義
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://railway.bulletinboard.techtrain.dev/threads?offset=1"
+        );
+        setThreads(response.data);
+      } catch (error) {
+        // エラーが発生した場合にエラーステートを更新
+        setError("データの取得に失敗しました。");
+        console.error(error);
+      }
+    };
+
+    // fetchData関数を呼び出し
+    fetchData();
+  }, []); // 空の依存配列を渡して初回レンダリング時のみ実行されるようにする
+
   return (
     <>
-      <div className="p-12 h-screen bg-gray-500">
-        <header className="text-center font-serif font-bold text-4xl">
-          掲示板
+      <div>
+        <header className=" bg-lime-400 py-3 flex justify-around">
+          <p className="text-center font-serif font-bold text-4xl">掲示板</p>
+          <button type="button" className="right-4 top-5">
+            スレッドを立てる
+          </button>
         </header>
+      </div>
+      <div className="px-12 h-screen bg-gray-500">
         <div className="text-center p-4">
           <p className="text-xl p-5">一覧</p>
           <ul>
             {threads.map((t) => (
-              <li className="font-bold p-2 cursor-pointer mb-2 bg-slate-200 rounded-md">
+              <li
+                key={t.id}
+                className="font-bold p-2 cursor-pointer mb-2 bg-slate-200 rounded-md"
+              >
                 {t.title}
               </li>
             ))}
