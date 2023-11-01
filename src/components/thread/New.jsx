e3254b0d341bbcb29cc3012fb0523f60563469a5
西村翔太 <nishimurasyota@k-syota.local>
2023-11-01 22:27:41 +0900

    :adhesive_bandage: スレッド作成の機能実装（改善余地あり）

--- src/components/thread/New.jsx
+++ src/components/thread/New.jsx
@@ -1,15 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const New = () => {
+  const [error, setError] = useState(null); // エラーステートを追加
+  const [inputTitle, setInputTitle] = useState("");
+
+  const fetchInputTitle = (e) => {
+    setInputTitle(e.target.value);
+    console.log(inputTitle);
+  };
+
+  const postData = async () => {
+    try {
+      const response = await axios
+        .post("https://railway.bulletinboard.techtrain.dev/threads", {
+          title: inputTitle,
+        })
+        .then((response) => {
+          alert("作成が完了しました");
+          setInputTitle("");
+        });
+    } catch (error) {
+      // エラーが発生した場合にエラーステートを更新
+      setError("データの取得に失敗しました。");
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <h2 className="text-2xl p-5 text-white">スレッド新規作成</h2>
       <form action="" method="post">
         <input
           type="text"
+          value={inputTitle}
           className="w-1/2 h-16 rounded-md px-10 text-lg"
           placeholder="タイトルを入力"
+          onChange={fetchInputTitle}
         />
       </form>
       <div className="mt-4 flex items-center justify-evenly">
@@ -21,6 +49,7 @@ const New = () => {
         </Link>
         <button
           type="submit"
+          onClick={postData}
           className="bg-white hover:bg-gray-300 p-2 duration-700 rounded-sm"
         >
           作成
