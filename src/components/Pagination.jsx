db5830df960d3253d678118b2718478bd18437b1
西村翔太 <nishimurasyota@k-syota.local>
2023-11-01 22:26:33 +0900

    :sparkles: ページネーションの作成

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Pagination = (props) => {
+  const { prev, next } = props;
+  return (
+    <>
+      <div className="flex justify-evenly">
+        <button
+          type="button"
+          onClick={prev}
+          className="bg-white hover:bg-gray-300 p-2 duration-700 rounded-sm"
+        >
+          前の１０件
+        </button>
+        <button
+          type="button"
+          onClick={next}
+          className="bg-white hover:bg-gray-300 p-2 duration-700 rounded-sm"
+        >
+          次の１０件
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Pagination;
