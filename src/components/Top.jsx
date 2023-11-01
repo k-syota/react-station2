2a4cf18cf1613b2c98b190cab55b7fb4f00d8235
西村翔太 <nishimurasyota@k-syota.local>
2023-11-01 22:27:18 +0900

    :adhesive_bandage: URLのoffset箇所をstate管理にする（暫定）

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -2,17 +2,19 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import React from "react";
 import ThreadList from "./ThreadList";
+import Pagination from "./Pagination";
 
 const Top = () => {
   const [threads, setThreads] = useState([]);
   const [error, setError] = useState(null); // エラーステートを追加
+  const [offset, setOffset] = useState(1);
 
   useEffect(() => {
     // 非同期関数を定義
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://railway.bulletinboard.techtrain.dev/threads?offset=1"
+          `https://railway.bulletinboard.techtrain.dev/threads?offset=${offset}`
         );
         setThreads(response.data);
       } catch (error) {
@@ -24,15 +26,27 @@ const Top = () => {
 
     // fetchData関数を呼び出し
     fetchData();
-  }, []); // 空の依存配列を渡して初回レンダリング時のみ実行されるようにする
+  }, [threads, offset]);
+
+  const prevOffset = () => {
+    if (offset > 10) {
+      setOffset(offset - 10);
+    }
+  };
+
+  const nextOffset = () => {
+    setOffset(offset + 10);
+  };
+
   return (
     <>
-      <p className="text-2xl p-5 text-white">一覧</p>
+      <p className="text-2xl p-5 text-white">スレッド一覧</p>
       <ul>
         {threads.map((t) => (
           <ThreadList key={t.id} title={t.title} />
         ))}
       </ul>
+      <Pagination prev={prevOffset} next={nextOffset} />
     </>
   );
 };
