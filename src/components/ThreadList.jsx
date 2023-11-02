304a723a7ba6cb38e6d2e821604beb6678374f25
西村翔太 <syota4723@gmail.com>
2023-11-02 15:16:50 +0900

    :sparkles: 対象スレッドの投稿一覧画面へのリンク設定

--- src/components/ThreadList.jsx
+++ src/components/ThreadList.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ThreadList = (props) => {
-  const { title } = props;
+  const { id, title } = props;
   return (
     <>
-      <li className="mx-auto font-bold p-2 cursor-pointer mb-2 bg-white rounded-md w-1/4 duration-500 hover:bg-blue-200">
-        {title}
-      </li>
+      <Link to={`/thread/${id}`} state={{ title: title }}>
+        <li className="mx-auto font-bold p-2 cursor-pointer mb-2 bg-white rounded-md w-1/4 duration-500 hover:bg-blue-200">
+          {title}
+        </li>
+      </Link>
     </>
   );
 };
