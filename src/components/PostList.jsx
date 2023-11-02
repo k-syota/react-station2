296fd1a4c1bb336d86e93e0ccd1471d1110136b1
西村翔太 <syota4723@gmail.com>
2023-11-02 21:27:06 +0900

    :sparkles: 新規投稿の画面作成

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,6 +5,7 @@ import { Link, useLocation, useParams } from "react-router-dom";
 const PostList = (props) => {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null); // エラーステートを追加
+  const [inputPost, setInputPost] = useState("");
   const params = useParams();
   const location = useLocation();
   const threadTitle = location.state.title;
@@ -15,7 +16,6 @@ const PostList = (props) => {
       try {
         const response = await axios.get(
           `https://railway.bulletinboard.techtrain.dev/threads/${params.id}/posts`
-          //   "https://railway.bulletinboard.techtrain.dev/threads/f9785a7e-837c-4dd5-a87b-833ca224a378/posts"
         );
         setPosts(response.data.posts);
       } catch (error) {
@@ -27,23 +27,73 @@ const PostList = (props) => {
 
     getPostList();
   }, [posts]);
+
+  const fetchInputPost = (e) => {
+    setInputPost(e.target.value);
+    console.log(inputPost);
+  };
+
+  const postData = async () => {
+    try {
+      const response = await axios
+        .post(
+          `https://railway.bulletinboard.techtrain.dev/threads/${params.id}/posts`,
+          {
+            post: inputPost,
+          }
+        )
+        .then((response) => {
+          alert("作成が完了しました");
+          setInputPost("");
+        });
+    } catch (error) {
+      // エラーが発生した場合にエラーステートを更新
+      setError("データの取得に失敗しました。");
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <p className="text-2xl p-5 text-white">
         スレッドのタイトル：{threadTitle}
       </p>
-      {posts.length > 0 ? (
-        posts.map((p) => (
-          <p
-            key={p.id}
-            className="mx-auto font-bold p-2 mb-2 bg-white rounded-md w-1/4"
+      <div className="flex">
+        <div className="w-1/2">
+          {posts.length > 0 ? (
+            posts.map((p) => (
+              <p
+                key={p.id}
+                className="mx-auto font-bold p-2 mb-2 bg-white rounded-md w-1/2"
+              >
+                {p.post}
+              </p>
+            ))
+          ) : (
+            <p className="text-white font-bold text-lg">投稿はありません</p>
+          )}
+        </div>
+        <div className="w-1/2">
+          <form action="" method="post">
+            <textarea
+              className="w-3/4 h-40 rounded-md p-4 text-lg"
+              type="text"
+              name=""
+              id=""
+              value={inputPost}
+              onChange={fetchInputPost}
+              placeholder="投稿しよう！"
+            />
+          </form>
+          <button
+            onClick={postData}
+            type="submit"
+            className="block bg-lime-400 hover:bg-lime-100 w-1/2 mx-auto mt-6 p-2 duration-700 rounded-sm"
           >
-            {p.post}
-          </p>
-        ))
-      ) : (
-        <p className="text-white font-bold text-lg">投稿はありません</p>
-      )}
+            投稿
+          </button>
+        </div>
+      </div>
       <Link
         to="/"
         className="block bg-white hover:bg-gray-300 w-1/2 mx-auto mt-20 p-2 duration-700 rounded-sm"
